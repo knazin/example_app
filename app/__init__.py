8c52b28518a8ef9b01d60a5b6d3de26796c20797
kacperknaz <knazkacper@gmail.com>
2019-07-12 14:04:03 +0200

    Add API documentation

--- app/__init__.py
+++ app/__init__.py
@@ -12,9 +12,17 @@ def update_app(application, config_name):
     # configuration
     app.config.from_object(config[config_name])
 
+    # swagger specific
+    SWAGGER_URL = '/api/v1/docs'
+    API_URL = '/static/swagger.yaml'
+    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(SWAGGER_URL, API_URL,
+        config={ 'app_name': "Python-Flask-REST"}
+    )
+    application.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
     return application
 
 
 @app.route('/')
 def index():
-    return "Hello"
+    return redirect('api/v1/docs')
