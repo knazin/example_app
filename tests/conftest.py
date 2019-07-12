fdb3d218f58bb03c07949a4850ae5db0e3582c80
kacperknaz <knazkacper@gmail.com>
2019-07-12 16:55:19 +0200

    Fix fitures

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,7 @@ import psycopg2
 from run import application, db
 from app.api.models import Text, Image
 
-# import app.tasks.tasks
-# from app.api.tasks import gettext
 from app.api.worker import celery
-#from app import app, db
-#from app.models import Ingredient, Recipe, Order
 
 from dotenv import load_dotenv
 load_dotenv(dotenv_path="../app/.env")
@@ -32,50 +28,18 @@ def test_client():
 
     ctx.pop()
 
-# @pytest.fixture(scope="module")
-# def test_client():
-#     # Flask provides a way to test your application by exposing the Werkzeug test Client
-#     # and handling the context locals for you.
-#     testing_client = app.test_client()
-
-#     # Establish an application context before running the tests.
-#     ctx = app.app_context()
-#     ctx.push()
-
-#     yield testing_client  # this is where the testing happens!
-
-#     ctx.pop()
 
 @pytest.fixture(scope="module") # module # session 
 def database():
-    # db.session.remove()
-    # db.reflect()
-    # db.drop_all()
+    db.session.remove()
+    db.reflect()
+    db.drop_all()
     db.create_all()
 
     yield db  # this is where the testing happens!
 
-    # db.drop_all()
+    db.drop_all()
 
-
-# @pytest.fixture(scope="module")
-# def database():
-#     # Create the database and the database table
-#     # db.create_all()
-
-#     # Insert user data
-#     # ing = Ingredient('name2', 50, 1000)
-#     # db.session.add(ing)
-
-#     # rec = Recipe('recipe2', [ing], '1')
-#     # db.session.add(rec)
-
-#     # Commit the changes for the users
-#     # db.session.commit()
-
-#     yield db  # this is where the testing happens!
-
-#     # db.drop_all()
 
 @pytest.fixture(scope="module")
 def f_celery():
@@ -95,13 +59,3 @@ def text():
 @pytest.fixture(scope="module")
 def image():
     yield Image
-
-
-# @pytest.fixture(scope="module")
-# def recipe():
-#     yield Recipe
-
-
-# @pytest.fixture(scope="module")
-# def order():
-#     yield Order
